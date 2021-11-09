0b0a8fcab6d524e16a45d4e27cf121ac32c9bf80
okkyprasetian <okkyprasetian@gmail.com>
2021-11-09 10:35:16 +0700

    add capitalizeMe function in Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,22 @@
 function Card({ obj }) {
+
+    const capitalizeMe = texts => {
+        texts = texts.split(' ')
+        for (let i = 0; i < texts.length; i++) {
+            texts[i] = texts[i].split('')
+            texts[i][0] = texts[i][0].toUpperCase()
+            texts[i] = texts[i].join('')
+        }
+        return texts.join(' ')
+    }
+
     return (
         <div className="card">
             <div className="card-img" style={{ backgroundImage: `url(${obj.img})` }}>
             </div>
             <div className="card-detail">
                 <div className="top-detail">
-                    <h3 className="title-detail">{obj.title}</h3>
+                    <h3 className="title-detail">{capitalizeMe(obj.title)}</h3>
                     <p className="price-detail">${obj.price}</p>
                 </div>
                 <div className="bottom-detail">
