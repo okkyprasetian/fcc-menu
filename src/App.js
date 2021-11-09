c4430cdfc54fe8808bc977e762b6fb1ee6d7b0ed
okkyprasetian <okkyprasetian@gmail.com>
2021-11-09 08:32:37 +0700

    add data.js to src and import it to app for initial state showMenu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { useState } from 'react'
+import menu from './data'
 import Header from './components/Header'
 import Nav from './components/Nav'
 import Cards from './components/Cards'
 
 function App() {
+
+  // State
+  const [showMenu, setShowMenu] = useState(menu)
+
   return (
     <div className="App">
       <Header />
